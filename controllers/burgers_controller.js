099812901619a4ab134ad1c019d4ea24fd8a5831
Joel Straley <joelstraley@gmail.com>
2021-02-13 01:03:08 -0500

    Updated Burgers_controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,14 +3,10 @@ const burger = require('../models/burger.js');
 
 const router = express.Router();
 
-// Import the model (cat.js) to use its database functions.
-const cat = require('../models/burger.js');
-
-// Create all our routes and set up logic within those routes where required.
 router.get('/', (req, res) => {
   burger.selectAll((data) => {
     const hbsObject = {
-      cats: data,
+      burgers: data,
     };
     console.log(hbsObject);
     res.render('index', hbsObject);
@@ -19,7 +15,6 @@ router.get('/', (req, res) => {
 
 router.post('/api/burgers', (req, res) => {
   burger.insertOne(['name', 'vegan'], [req.body.name, req.body.vegan], (result) => {
-    // Send back the ID of the new quote
     res.json({ id: result.insertId });
   });
 });
@@ -36,7 +31,6 @@ router.put('/api/burgers/:id', (req, res) => {
     condition,
     (result) => {
       if (result.changedRows === 0) {
-        // If no rows were changed, then the ID must not exist, so 404
         return res.status(404).end();
       }
       res.status(200).end();
@@ -49,12 +43,10 @@ router.deleteOne('/api/burgers/:id', (req, res) => {
 
   burger.deleteOne(condition, (result) => {
     if (result.affectedRows === 0) {
-      // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
     }
     res.status(200).end();
   });
 });
 
-// Export routes for server.js to use.
 module.exports = router;
